e64645004eb4bcb602fcd7f10fdd9cfed90e2f01
JorgePastorCastilla <jordi131313@gmail.com>
2020-02-05 17:53:34 +0100

    :rocket:

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Biblioteca import Biblioteca
 from model.Obra import Obra
 from model.Publicacion import Publicacion
+from model.Revista import Revista
 from model.Volum import Volum
 
 input = ""
@@ -30,7 +31,60 @@ def ask_create_options():
     print("4. Revista.")
     createInput = str( raw_input("Elige una opcion: \n"))
     separador()
+    if(createInput == "1"):
+        create_Publicacion()
+    elif(createInput == "2"):
+        create_Obra()
+    elif(createInput == "3"):
+        create_Volum()
+    elif(createInput == "4"):
+        create_Revista()
+    elif(input == ""):
+        print("Tienes que introducir una opcion")
+        separador()
+        ask_create_options()
+    else:
+        print("Esto no es una opcion correcta")
+        separador()
+        ask_create_options()
 def create_Publicacion():
+    referencia = str(raw_input("Introduce la referencia: \n"))
+    separador()
+    titol = str(raw_input("Introduce el titulo: \n"))
+    separador()
+    biblioteca.add_publicacion(Publicacion(referencia=referencia,titol=titol))
+def create_Obra():
+    referencia = str(raw_input("Introduce la referencia: \n"))
+    separador()
+    titol = str(raw_input("Introduce el titulo: \n"))
+    separador()
+    autor = str(raw_input("Introduce el autor: \n"))
+    separador()
+    nrePags = str(raw_input("Introduce el numero de paginas: \n"))
+    separador()
+    biblioteca.add_publicacion(Obra(referencia=referencia,titol=titol,autor=autor,nrePags=nrePags))
+def create_Volum():
+    referencia = str(raw_input("Introduce la referencia: \n"))
+    separador()
+    titol = str(raw_input("Introduce el titulo: \n"))
+    separador()
+    autor = str(raw_input("Introduce el autor: \n"))
+    separador()
+    nrePags = str(raw_input("Introduce el numero de paginas: \n"))
+    separador()
+    nro = str(raw_input("Introduce el numero de publicaciones: \n"))
+    separador()
+    biblioteca.add_publicacion(Volum(referencia=referencia,titol=titol,autor=autor,nrePags=nrePags,nro=nro))
+def create_Revista():
+    referencia = str(raw_input("Introduce la referencia: \n"))
+    separador()
+    titol = str(raw_input("Introduce el titulo: \n"))
+    separador()
+    nro = str(raw_input("Introduce el numero de publicaciones: \n"))
+    separador()
+    any = str(raw_input("Introduce el anyo: \n"))
+    separador()
+    biblioteca.add_publicacion(Revista(referencia=referencia,titol=titol,any=any,nro=nro))
 
 
 while(not(input.__eq__("6"))):
@@ -45,11 +99,13 @@ while(not(input.__eq__("6"))):
         separador()
     elif(input == "3"):
         #aqui van lo de anadir publicaciones
-        publicacion = Volum("20","paco","200","ref2","titulo")
+        # publicacion = Volum("20","paco","200","ref2","titulo")
         #pregunta = str( raw_input("Cual es la referencia? ") )
-        biblioteca.add_publicacion(publicacion)
+        # biblioteca.add_publicacion(publicacion)
+        ask_create_options()
     elif(input == "4"):
         pregunta = str( raw_input("Cual es la referencia? \n") )
+        separador()
         biblioteca.find_publicacion(pregunta).visualitzar()
         separador()
     elif(input == "5"):
